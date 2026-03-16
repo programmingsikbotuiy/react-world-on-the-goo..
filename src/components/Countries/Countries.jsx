291b8c319b70951e97b3df686d4873cae008ae85
programmingsikbotuiy <susmitaswanaker@gmail.com>
2026-03-16 19:36:39 +0600

    This is my first react projects setup

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h3>React world on the go....</h3>
+        </div>
+    );
+};
+
+export default Countries;
